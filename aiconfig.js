5cfb649630f08151c2306bb58ed864e303f50c53
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2024-06-20 12:28:38 +0200

    Update aiconfig.js

--- aiconfig.js
+++ aiconfig.js
@@ -2,112 +2,59 @@ export const OPENAI_API_KEY = 'sk-proj-ieiqFT6HuZQ1liEeCGm9T3BlbkFJ069jEZVSq1FjS
 export const OPENAI_INSTRUCTIONS_SYSTEM = 'You are an expert in creating forms for specific scenarios.' +
   'If the user prompt makes no sense, contains illegal material or is in any case not suitable, return an empty JSON,' +
   ' otherwise follow this master form as an example, find a suitable formName and output JSON:\n' +
-  '[\n' +
-  '  {\n' +
-  '    "label": "First name",\n' +
-  '    "name": "firstname",\n' +
-  '    "type": "single-line-text",\n' +
-  '    "validation": {\n' +
-  '      "message": "Validation message first name",\n' +
-  '      "pattern": ".*",\n' +
-  '      "isRequired": true\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Comment",\n' +
-  '    "name": "comment",\n' +
-  '    "type": "multi-line-text",\n' +
-  '    "validation": {\n' +
-  '      "message": "Validation message last name",\n' +
-  '      "pattern": ".*",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Test label",\n' +
-  '    "name": "label1",\n' +
-  '    "type": "label"\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "checkbox1",\n' +
-  '    "type": "checkbox",\n' +
-  '    "options": [\n' +
-  '      "Option 1",\n' +
-  '      "Option 2",\n' +
-  '      "Option 3"\n' +
-  '    ],\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid input",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "radio1",\n' +
-  '    "type": "radio",\n' +
-  '    "options": [\n' +
-  '      "Option 1",\n' +
-  '      "Option 2",\n' +
-  '      "Option 3"\n' +
-  '    ],\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid input",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "date1",\n' +
-  '    "type": "date",\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid input",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "dropdown1",\n' +
-  '    "type": "dropdown",\n' +
-  '    "options": [\n' +
-  '      "Option 1",\n' +
-  '      "Option 2",\n' +
-  '      "Option 3"\n' +
-  '    ],\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid input",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "dropdown2",\n' +
-  '    "type": "multi-select-dropdown",\n' +
-  '    "options": [\n' +
-  '      "Option 1",\n' +
-  '      "Option 2",\n' +
-  '      "Option 3"\n' +
-  '    ],\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid input",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  },\n' +
-  '  {\n' +
-  '    "label": "Enter label text",\n' +
-  '    "name": "fileupload",\n' +
-  '    "type": "file-upload",\n' +
-  '    "options": [\n' +
-  '      "pdf",\n' +
-  '      "doc",\n' +
-  '      "txt",\n' +
-  '      "jpg"\n' +
-  '    ],\n' +
-  '    "validation": {\n' +
-  '      "message": "Invalid file",\n' +
-  '      "isRequired": false\n' +
-  '    }\n' +
-  '  }\n' +
-  ']'
+  `{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+  "fields": {
+    "type": "array",
+      "items": [
+      {
+        "type": "object",
+        "properties": {
+          "label": {
+            "type": "string"
+          },
+          "name": {
+            "type": "string"
+          },
+          "type": {
+            "type": "string",
+            "enum": ["single-line-text", "multi-line-text", "label", "checkbox", "radio", "date", "dropdown", "file-upload"]
+          },
+          "validation": {
+            "type": "object",
+            "properties": {
+              "message": {
+                "type": "string"
+              },
+              "pattern": {
+                "type": "string"
+              },
+              "isRequired": {
+                "type": "boolean"
+              }
+            },
+            "required": ["message", "pattern", "isRequired"]
+          }
+        },
+        "required": ["label", "name", "type", "validation"],
+        "properties": {
+          "options": {
+            "type": "array",
+            "items": {
+              "type": "string"
+            }
+          }
+        }
+      }
+    ]
+  },
+  "formName": {
+    "type": "string"
+  }
+}
+}`;
+
 
 
