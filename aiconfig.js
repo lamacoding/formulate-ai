4bdd24510b489e472432162d1a48063f6d2c4ba2
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2024-07-21 21:40:49 +0200

    Update aiconfig.js

--- aiconfig.js
+++ aiconfig.js
@@ -1,4 +1,4 @@
-export const OPENAI_API_KEY = 'sk-proj-ieiqFT6HuZQ1liEeCGm9T3BlbkFJ069jEZVSq1FjSwdHJbKk';
+export const OPENAI_API_KEY = 'sk-proj-ieiqFT6HuZQ1liEeCGm9T3BlbkFJ069jEZVSq1FjSwdHJbKk'; // This key does not work, obviously
 export const OPENAI_INSTRUCTIONS_SYSTEM = 'You are an expert in creating forms for user given scenarios.' +
   'If the user prompt makes no sense, contains illegal material or is in any case not suitable, return an empty JSON,' +
   ' otherwise follow this master form as an example, find a suitable formName and output JSON:\n' +
